2cc53d3f1863546587d23078e68ab25c60deec66
Guillén <domingofestivo@gmail.com>
2026-08-05 09:48:48 +0200

    chore: accesslog a stderr para que Railway capture las lineas de access log

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,9 @@
+import sys
+
 bind = "0.0.0.0:8080"
 workers = 3
 timeout = 60
-accesslog = "/tmp/gunicorn-access.log"
+accesslog = sys.stderr
 access_log_format = '%(h)s "%(r)s" %(s)s %(D)sus'
 errorlog = "-"
-loglevel = "debug"
+loglevel = "info"
