46ee9f485a14fa31169e06b9ef28bcc281200d20
Guillén <domingofestivo@gmail.com>
2026-08-05 09:47:19 +0200

    chore: debug — access log a archivo + loglevel debug

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,7 @@
 bind = "0.0.0.0:8080"
 workers = 3
 timeout = 60
-accesslog = "-"
+accesslog = "/tmp/gunicorn-access.log"
 access_log_format = '%(h)s "%(r)s" %(s)s %(D)sus'
 errorlog = "-"
+loglevel = "debug"
